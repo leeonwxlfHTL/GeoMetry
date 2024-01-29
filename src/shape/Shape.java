27c504c3311007a0ac54fd84856707bfef807373
leeonwxlfHTL <leon.wolf@hak-reutte.ac.at>
2024-01-29 15:58:13 +0100

    Create and write this Class

--- src/shape/Shape.java
+++ src/shape/Shape.java
@@ -1,5 +1,38 @@
 package shape;
 
 public abstract class Shape {
+	
+	private double xCoords;
+	private double yCoords;
+	
+	public Shape(double xCoords, double yCoords) {
+		super();
+		this.xCoords = xCoords;
+		this.yCoords = yCoords;
+	}
 
+	public double getxCoords() {
+		return xCoords;
+	}
+
+	public void setxCoords(double xCoords) {
+		this.xCoords = xCoords;
+	}
+
+	public double getyCoords() {
+		return yCoords;
+	}
+
+	public void setyCoords(double yCoords) {
+		this.yCoords = yCoords;
+	}
+	
+	
+
+	
+	public abstract double calculateCircumference();
+	
+	public abstract double calculateArea();
+
+	
 }
