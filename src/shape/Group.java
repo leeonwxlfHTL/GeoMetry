71188857bded33276962ff415582c0e90be447c2
leeonwxlfHTL <leon.wolf@hak-reutte.ac.at>
2024-01-29 16:01:01 +0100

    write all the methods

--- src/shape/Group.java
+++ src/shape/Group.java
@@ -1,5 +1,46 @@
 package shape;
 
+import java.util.ArrayList;
+
 public class Group {
+
+	private ArrayList<Shape> shapes;
+
+	public double calculateAllArea() {
+		double sum = 0;
+		for (Shape s : shapes) {
+			sum += s.calculateArea();
+		}
+		return sum;
+
+	}
+
+	public double calculateAllCirumference() {
+		double sum = 0;
+		for (Shape s : shapes) {
+			sum += s.calculateCircumference();
+		}
+		return sum;
+
+	}
+
+	public void printAllShapes() {
+		for (Shape s : shapes) {
+			System.out.println(s);
+		}
+
+	}
+
+	public Shape findBiggestShape() {
+		Shape biggestShape = null;
+		for (Shape s : shapes) {
+			double current = s.calculateArea();
+			if (current > biggestShape.calculateArea()) {
+				biggestShape = s;
+			}
+
+		}
+		return biggestShape;
+	}
 
 }
